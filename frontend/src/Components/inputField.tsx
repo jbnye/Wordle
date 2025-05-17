577a761d7ff134d864d2c376ea722f18d1ee8725
Jbnye <jbnye@umich.edu>
2025-05-16 21:49:54 -0400

    redid front end seperating from back end

---
+++ frontend/src/Components/inputField.tsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+
+interface InputFieldProps {
+    guess: string;
+    setGuess: (guess: string) => void;
+};
+
+
+export default function InputField({guess, setGuess}: InputFieldProps){
+
+const  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredValue = e.target.value.replace(/[^A-Za-z]/g, '').toUpperCase();
+    const newGuess = filteredValue.slice(0,5);
+    setGuess(newGuess);
+}
+
+return (
+
+    <input type="text" value = {guess} onChange={handleChange} maxLength={5} className="border-2 border-gray-400 p-2 text-xl uppercase">
+    
+    </input>
+
+);
+
+}
