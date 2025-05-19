a73a2a3fddf2b93e2dda1873e5c91c12fd8f88b4
Jacob Nye <jn3268@gmail.com>
2025-05-18 20:55:05 -0400

    completed basic functionality of game/inputField/rows squares and colors

--- frontend/src/Components/Squares.tsx
+++ frontend/src/Components/Squares.tsx
@@ -2,16 +2,36 @@ import {useState} from "react";
 
 
 interface SquareProps{
-    value: string | null;
-    length?: never;
+    value: string,
+    status: string,
 }
 
-export default function Squares({value}: SquareProps){
+export default function Squares({value, status}: SquareProps){
     
 
     return(
-        <div className="w-[62px] h-[62px] border-2 border-gray-400 flex items-center justify-center text-[30px]">
-            {value || ""}
-        </div>
+        <>
+            {status === "absent" ? (
+            <div className="w-[62px] h-[62px] border-2 border-gray-400 bg-[rgb(120,124,126)] flex items-center justify-center text-[30px] text-white font-sans font-bold tracking-wide rounded-[4px]">
+                {value || ""}
+            </div>
+            ): status === "wrong" ? (
+            <div className="w-[62px] h-[62px] border-2 border-gray-400 bg-[rgb(201,180,88)] flex items-center justify-center text-[30px] text-white font-sans font-bold tracking-wide rounded-[4px]">
+                {value || ""}
+            </div>
+            ): status === "correct" ? (
+            <div className="w-[62px] h-[62px] border-2 border-gray-400 bg-[rgb(106,170,100)] flex items-center justify-center text-[30px] text-white font-sans font-bold tracking-wide rounded-[4px]">
+                {value || ""}
+            </div> 
+            ): value !== "" ? (
+            <div className="w-[62px] h-[62px] border-2 border-black-400  flex items-center justify-center text-[30px] font-sans font-bold tracking-wide rounded-[4px]">
+                {value || ""}
+            </div> 
+            ):
+            <div className="w-[62px] h-[62px] border-2 border-gray-400  flex items-center justify-center text-[30px] font-sans font-bold tracking-wide rounded-[4px]">
+                {value || ""}
+            </div> 
+            }
+        </>
     );
 }
