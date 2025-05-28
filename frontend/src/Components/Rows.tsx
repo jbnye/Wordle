123227b965f8e2f3db0d6fe2a99e2099a1218ee9
Jacob Nye <jn3268@gmail.com>
2025-05-27 23:07:26 -0400

    added flipping for squares and drilled through the rows, and added an index for each square

--- frontend/src/Components/Rows.tsx
+++ frontend/src/Components/Rows.tsx
@@ -5,11 +5,11 @@ interface RowsProps {
     data: string,
     letters: string[],
     shake?: boolean
-
+    flipTrigger?: boolean
 }
 
 
-export default function Rows({data, letters, shake}: RowsProps){
+export default function Rows({data, letters, shake, flipTrigger}: RowsProps){
     const [shouldShake, setShouldShake] = useState(false);
 
     useEffect(() => {
@@ -17,19 +17,22 @@ export default function Rows({data, letters, shake}: RowsProps){
         setShouldShake(true);
         const timer = setTimeout(() => {
             setShouldShake(false);
-        }, 500); // match your animation duration
+        }, 500); 
         return () => clearTimeout(timer);
         }
     }, [shake]);
 
     return(
         <>
-        <div className={`flex gap-2 ${shouldShake ? 'animate-[var(--animate-shake)]' : ''}`}>
+        <div className={`flex gap-2 perspective-[1000px] ${shouldShake ? 'animate-[var(--animate-shake)]' : ''}`}>
         {Array.from({ length: 5 }).map((_, i) => (
             <Squares
             key={i}
             value={data[i] || ""}
             status={letters[i] || ""}
+            delay={i * 300}
+            flipTrigger={flipTrigger}
+            index={i}
             />
         ))}
         </div>
